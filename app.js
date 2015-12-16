e802d942e33f6b7f9b7f207d20e93aac582b3f7e
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2015-12-15 21:09:44 -0800

    Missing semicolon

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 app.get("/about", function (request, response) {
-  response.end("About Me page!")
+  response.end("About Me page!");
 });
 
 app.use(function (request, response) {
