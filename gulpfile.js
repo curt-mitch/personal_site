2e7844ebbb0acac6ee4688dbf8b4eff32206df2b
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-01-31 10:01:50 -0800

    Nodemon and related Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 var stylus = require('gulp-stylus');
 
 gulp.task('watch', function () {
   gulp.watch('public/*.styl', ['styl']);
+});
+
+gulp.task('nodemon', function () {
+  nodemon({
+    script: 'app.js',
+    ext: 'js html',
+    env: { 'NODE_ENV': 'development' }
+  });
 });
 
 gulp.task('styl', function () {
