8b7c7147011accb46335a20f8b1a7a7953f2a2c0
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-12-31 13:39:16 -0600

    About Me static html page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var logger = require('morgan');
 var path = require('path');
 var http = require('http');
+var fs = require('fs');
 var helmet = require('helmet');
 
 var app = express();
@@ -14,13 +15,19 @@ app.use(helmet());
 var publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
-app.get('/about', function (request, response) {
-  response.end('About Me page!');
+app.get('/about.html', function (req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 200;
+
+  var file = fs.createReadStream(req.url);
+  file.on('open', function () {
+    file.pipe(res);
+  });
 });
 
-app.use(function (request, response) {
-  response.statusCode = 404;
-  response.end('404 page');
+app.use(function (req, res) {
+  res.statusCode = 404;
+  res.end('404 page');
 });
 
 http.createServer(app).listen(3000);
