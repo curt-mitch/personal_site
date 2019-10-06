596cf1fa90438c345699463bd0798025ff0bdb04
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2019-10-06 16:02:47 -0700

    Basic middleware

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,20 @@ import * as Koa from 'koa';
 import * as Router from 'koa-router';
 
 const app = new Koa();
-const router = new Router();
 
+app.use(async (ctx, next) => {
+    // Log the request to the console
+    console.log('Url:', ctx.url);
+    // Pass the request to the next middleware function
+    await next();
+});
+
+const router = new Router();
 router.get('/*', async (ctx) => {
     ctx.body = 'Hello World!';
 });
-
 app.use(router.routes());
 
 app.listen(3000);
 
-// tslint:disable-next-line
 console.log('Server running on port 3000');
