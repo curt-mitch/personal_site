60510227fd25304736dd9bf535a219498c3665bc
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-05-03 14:45:00 -0700

    Install and use axios for API calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    hello: null,
+  };
+
+  componentDidMount() {
+    axios.get('/api/hello')
+      .then(res => this.setState({ hello: res.data }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.hello
+          ? <div> { this.state.hello } </div>
+          : null }
+      </div>
+    );
+  }
 }
 
 export default App;
