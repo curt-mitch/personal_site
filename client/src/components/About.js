5621b3614bb0fe96f44b75ca42ff8b222019de10
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-05-23 14:04:13 -0700

    Add content to about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -119,7 +119,24 @@ class About extends Component {
         <CssBaseline />
         <Topbar />
         <div className={classes.root}>
-          About Me
+          <Typography variant='body1' className={classes.block} >
+            I’m a software developer based in the San Francisco Bay Area. I’ve worked at a variety of analytics- and machine learning-startups as a web developer, and now I’m pursing Springboard’s machine learning engineering track as another pivot in my career path of using technology to make complex data more comprehensible.
+          </Typography>
+          <Typography variant='h5' className={classes.block} >
+            Background &#38; Side Projects
+          </Typography>
+          <Typography variant='body1' className={classes.block} >
+            I transitioned to software development after spending several years as a data analyst at an alternative energy and efficiency consultancy. I became a member of one of the first cohorts of the coding boot camp Hack Reactor in 2013 and since then have worked at various startups, primarily as a front-end engineer. Now I’m studying machine learning engineering in one of the first cohorts of that program via Springboard. I obtained a bachelor of science degree in physics and a bachelor of arts degree in math along with minors in German and Japanese from the University of North Texas.
+          </Typography>
+          <Typography variant='body1' className={classes.block} >
+            More recently I’ve been working on courses and side projects in machine learning (especially deep learning), mathematics (especially statistics) and natural language processing. This website gives a place to write about and showcase that work as well as tinker with JavaScript libraries and web development practices.
+          </Typography>
+          <Typography variant='body1' className={classes.block} >
+            Outside of the worlds of software and analytics my hobbies include reading all categories of books, studying foreign languages, cooking, martial arts and general exercise, playing guitar, and pursuing the everlasting struggle to find a balance between the desire to enjoy delicious foods and drinks with the desire to be fit and healthy.
+          </Typography>
+          <Typography variant='body1' className={classes.block} >
+          For more information on my work, to get in touch regarding employment opportunities, or to just say hello, feel free to get in touch at curtis.l.mitchell AT gmail.com.
+          </Typography>
         </div>
       </React.Fragment>
     );
