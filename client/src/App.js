f39f1a7607266eeff099a27e0a85a8ac2f757dd3
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-07-09 09:06:25 -0700

    Comment-out Axios (unused var) for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { ThemeProvider } from '@material-ui/styles';
 import './App.css';
 import Routes from './routes'
