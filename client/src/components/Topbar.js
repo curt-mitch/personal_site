5ba4330d80aa1a37270355894137c7bdc7ee84f7
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-08-21 15:01:01 -0700

    Add temp logos

--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -16,7 +16,7 @@ import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 import { Link as MaterialLink } from "@material-ui/core";
 import Menu from "./Menu";
 
-const logo = require("../images/logo.svg");
+const logo = require("../images/curt-mitch-logo.svg");
 
 const styles = theme => ({
   appBar: {
