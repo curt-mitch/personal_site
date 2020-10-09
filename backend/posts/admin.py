101213d8d388d6a8fabbcab1d6e12cd7b1ff1fb5
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-10-09 16:16:15 -0700

    Initial posts schema

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Posts
+
+class PostsAdmin(admin.ModelAdmin):
+  list_display = ('title', 'first_sentence', 'publish_date', 'post_link')
 
 # Register your models here.
+admin.site.register(Posts, PostsAdmin)
