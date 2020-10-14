acc2b48f743f3610bdf18f14b01abbaa92e6cefb
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-10-14 16:49:01 -0700

    Initial JP EN Translator page

---
+++ client/src/projects/JPENTranslator.js
@@ -0,0 +1,151 @@
+import React, { Component } from "react";
+import withStyles from "@material-ui/styles/withStyles";
+import { withRouter } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+
+import Topbar from "../components/Topbar";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.grey["100"],
+    overflow: "hidden",
+    backgroundSize: "cover",
+    backgroundPosition: "0 400px",
+    paddingBottom: 200
+  },
+  grid: {
+    width: 1200,
+    marginTop: 40,
+    [theme.breakpoints.down("sm")]: {
+      width: "calc(100% - 20px)"
+    }
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: "left",
+    color: theme.palette.text.secondary
+  },
+  rangeLabel: {
+    display: "flex",
+    justifyContent: "space-between",
+    paddingTop: theme.spacing(2)
+  },
+  topBar: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 32
+  },
+  outlinedButtom: {
+    textTransform: "uppercase",
+    margin: theme.spacing(1)
+  },
+  actionButtom: {
+    textTransform: "uppercase",
+    margin: theme.spacing(1),
+    width: 152
+  },
+  blockCenter: {
+    padding: theme.spacing(2),
+    textAlign: "center"
+  },
+  block: {
+    padding: theme.spacing(2)
+  },
+  box: {
+    marginBottom: 40,
+    height: 65
+  },
+  inlining: {
+    display: "inline-block",
+    marginRight: 10
+  },
+  buttonBar: {
+    display: "flex"
+  },
+  alignRight: {
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  noBorder: {
+    borderBottomStyle: "hidden"
+  },
+  loadingState: {
+    opacity: 0.05
+  },
+  loadingMessage: {
+    position: "absolute",
+    top: "40%",
+    left: "40%"
+  },
+  jpInput: {
+    padding: '0px 48px',
+    margin: '8px'
+  },
+  paragraph: {
+    padding: theme.spacing(2),
+    textIndent: '20px',
+    textAlign: 'justify',
+  },
+});
+
+class JPENTranslator extends Component {
+  state = {
+    learnMoredialog: false,
+    getStartedDialog: false
+  };
+
+  componentDidMount() {}
+
+  openDialog = event => {
+    this.setState({ learnMoredialog: true });
+  };
+
+  dialogClose = event => {
+    this.setState({ learnMoredialog: false });
+  };
+
+  openGetStartedDialog = event => {
+    this.setState({ getStartedDialog: true });
+  };
+
+  closeGetStartedDialog = event => {
+    this.setState({ getStartedDialog: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Topbar />
+        <div className={classes.root}>
+          <Typography variant='h3' className={classes.block} >
+            Japanese-To-English Translation Application
+          </Typography>
+          <Typography variant='body1' className={classes.paragraph} >
+            This application takes in a Japanese sentence and returns a predicted English translation of that sentence. It was trained on the <Link color="secondary" underline="hover" href="https://nlp.stanford.edu/projects/jesc/">JESC Japanese-English Subtitle Corpus</Link> using an encoder-decoder neural network. To read more about the design and training of the model please read my post here.
+          </Typography>
+          <TextField
+            className={classes.jpInput}
+            placeholder="こんにちは"
+            helperText="Add Japanese text here"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withRouter(withStyles(styles)(JPENTranslator));
