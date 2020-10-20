a77a6c66f82d29868ac9a149ec2766b107b68129
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-10-20 13:57:30 -0700

    Clean up App code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
 import { ThemeProvider } from '@material-ui/styles';
 import './App.css';
 import Routes from './routes'
 import theme from './theme';
 
 class App extends Component {
-
-  state = {
-    hello: null,
-  };
-
-  componentDidMount() {
-    // axios.get('/api/hello')
-    //   .then(res => this.setState({ hello: res.data }))
-    //   .catch(err => console.error(err));
-  }
 
   render() {
     return (
