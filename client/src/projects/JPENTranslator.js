83b160845a2bce89a0599163216ef7deee6db750
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-10-26 12:18:35 -0700

    Update link to Google translate

--- client/src/projects/JPENTranslator.js
+++ client/src/projects/JPENTranslator.js
@@ -195,7 +195,7 @@ class JPENTranslator extends Component {
             This application takes in a Japanese sentence and returns a predicted English translation of that sentence. It was trained on the <Link color="secondary" underline="hover" href="https://nlp.stanford.edu/projects/jesc/">JESC Japanese-English Subtitle Corpus</Link> using an encoder-decoder neural network.
           </Typography>
           <Typography variant='body1' className={classes.paragraph} >
-            Not a Japanese-speaker? Just click this link to use Google Translate to create and copy some Japanese text to paste back here: <Link color="secondary" underline="hover" href="https://translate.google.com/#view=home&op=translate&sl=en&tl=ja">EN-JP Google Translate</Link>
+            Not a Japanese-speaker? Just click this link to got to Google Translate where you can create and copy some Japanese text to paste back here: <Link color="secondary" underline="hover" target="_blank" rel="noopener noreferrer" href="https://translate.google.com/#view=home&op=translate&sl=en&tl=ja">EN-JP Google Translate</Link>
           </Typography>
           <div className={classes.jpInputContainer}>
             <TextField
