c769fbc8816596c0ec802a016a05aade9035e7a0
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-10-29 11:57:38 -0700

    Tests for posts and projects classes

--- backend/apps/posts/tests.py
+++ backend/apps/posts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from apps.posts.models import Posts
+import datetime
 
-# Create your tests here.
+class PostsTestCase(TestCase):
+    def setUp(self):
+        Posts.objects.create(
+            title = 'First Post',
+            first_sentence = 'first post sentence',
+            publish_date = datetime.datetime(2020, 7, 15),
+            post_link = '/link/to/post1'
+        )
+        Posts.objects.create(
+            title = 'Second Post',
+            first_sentence = 'second post sentence',
+            publish_date = datetime.datetime(2020, 9, 15),
+            post_link = '/link/to/post2'
+        )
+
+    def test_posts_return_title(self):
+        """Posts return own titles"""
+        post1 = Posts.objects.get(title="First Post")
+        post2 = Posts.objects.get(title="Second Post")
+        self.assertEqual(post1._str_(), 'First Post')
+        self.assertEqual(post2._str_(), 'Second Post')
--- backend/apps/projects/tests.py
+++ backend/apps/projects/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from apps.projects.models import Projects
+import datetime
 
-# Create your tests here.
+class PostsTestCase(TestCase):
+    def setUp(self):
+        Projects.objects.create(
+            title = 'Project 1',
+            description = 'description of first project',
+            publish_date = datetime.datetime(2020, 8, 15),
+            project_link = '/link/to/project1'
+        )
+        Projects.objects.create(
+            title = 'Project 2',
+            description = 'description of second project',
+            publish_date = datetime.datetime(2020, 10, 15),
+            project_link = '/link/to/project2'
+        )
+
+    def test_projects_return_title(self):
+        """Projects return own titles"""
+        project1 = Projects.objects.get(title="Project 1")
+        project2 = Projects.objects.get(title="Project 2")
+        self.assertEqual(project1._str_(), 'Project 1')
+        self.assertEqual(project2._str_(), 'Project 2')
