c6a3c20958db53f10552761438f474e7d674f9d1
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2021-01-14 06:27:48 -0800

    Temp comment out uhat views to deal with PIL import

--- backend/apps/urdu_letter_endpoints/views.py
+++ backend/apps/urdu_letter_endpoints/views.py
@@ -1,5 +1,5 @@
-import json
-from PIL import Image
+import uuid
+# from PIL import Image
 import numpy as np
 
 from rest_framework import generics
@@ -20,35 +20,45 @@ class Prediction(generics.RetrieveAPIView):
     parser_classes = [JSONParser, MultiPartParser, FormParser]
 
     def post(self, request, *args, **kwargs):
-        input_image = request.data['letter.jpg']
-        input_data = self.convert_image_to_npdata(input_image)
+        # input_image = request.data['letter.jpg']
+        # input_data = self.convert_image_to_npdata(input_image)
         predictions = []
 
-        try:
-            predictions = self.create_prediction_response(input_data)
-        except Exception as exception:
-            predictions = {"error": exception.__class__.__name__}
+        # try:
+        #     predictions = self.create_prediction_response(input_data)
+        # except Exception as exception:
+        #     predictions = {"error": exception.__class__.__name__}
         response = {"predictions": predictions}
 
         return JsonResponse(response, safe=False)
 
-    def convert_image_to_npdata(self, input_image):
-        img = Image.open(input_image)
-        size = 28, 28
-        img.thumbnail(size)
-        npdata = np.asarray(img)
-        npdata = npdata[:, :, [0]] / 255.
-        npdata = np.expand_dims(npdata, 0)
-        return npdata
+    # def convert_image_to_npdata(self, input_image):
+    #     img = Image.open(input_image)
+    #     size = 28, 28
+    #     img = img.resize((28, 28), Image.ANTIALIAS)
+    #     img.save('img.png')
+    #     npdata = np.asarray(img)
+    #     whiten_and_normalize = lambda x: 1 if 0 < x <= 255 else 0
+    #     print('whiten')
+    #     vectorized_whiten = np.vectorize(whiten_and_normalize)
+    #     print('vectorized_whiten')
+    #     npdata = vectorized_whiten(npdata)
+    #     print(npdata)
+    #     npdata = (npdata[:, :, [0]])
+    #     for row in npdata[:, :, [0]]:
+    #         print(row)
+    #     npdata = np.expand_dims(npdata, 0)
+    #     return npdata
 
-    def create_prediction_response(self, input_data):
-        # initialize prediction model
-        model = Model()
-        results = model.get_prediction(input_data)
-        response_list = []
-        for result in results:
-            char_map = uhat_map[result[0]]
-            char_map["percentage"] = str(result[1])
-            response_list.append(char_map)
+    # def create_prediction_response(self, input_data):
+    #     # initialize prediction model
+    #     model = Model()
+    #     results = model.get_prediction(input_data)
+    #     response_list = []
+    #     for result in results:
+    #         char_map = uhat_map[result[0]]
+    #         char_map["percentage"] = str("%.2f" % result[1])
+    #         char_map["uuid"] = str(uuid.uuid4())
+    #         response_list.append(char_map)
 
-        return response_list
+    #     return response_list
