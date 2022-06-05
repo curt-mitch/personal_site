3dd675dc943d3ba0e406641073f4eeb8d73f0b43
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2022-06-05 13:19:53 -0700

    Add Font Awesome modules and config

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-okaidia.css';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import { config } from '@fortawesome/fontawesome-svg-core';
 import type { AppProps } from 'next/app';
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
