bb2694422fd199066a14ed2a0df8975e0670fe2b
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2022-07-30 17:19:26 -0700

    Import CssBaseline at _app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,16 @@
 import '../styles/globals.css';
-import '../styles/Home.module.css';
-import '../styles/utils.module.css';
 import 'prismjs/themes/prism-okaidia.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
 import { config } from '@fortawesome/fontawesome-svg-core';
 import type { AppProps } from 'next/app';
+import { CssBaseline } from '@mui/material';
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
