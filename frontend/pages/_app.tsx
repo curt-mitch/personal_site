a4c534fb03503709ec157e6a7b61cb1a8fbad42e
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2022-09-10 08:50:19 -0700

    Rename global Sass file

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.scss';
+import '../styles/Global.scss';
 import 'prismjs/themes/prism-okaidia.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
 import { config } from '@fortawesome/fontawesome-svg-core';
